882731c6f52365338d39a44cec1360f344ab768d
Gevorg Arzumanjan <gevorg.arzumanjan@shiwaforce.com>
2019-06-12 19:55:32 +0200

    styling the modal elements

--- modal/modal.js
+++ modal/modal.js
@@ -18,16 +18,52 @@ class Modal extends HTMLElement {
                     background: white;
                     border-radius: 3px;
                     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-                    height: 30rem;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
                     left: 25%;
                     position: fixed;
                     top: 15vh;
                     width: 50%;
                     z-index: 100;
                 }
+
+                header {
+                    padding: 1rem;
+                }
+
+                header h1 {
+                    font-size: 1.25rem;
+                }
+
+                #main {
+                    padding: 1rem;
+                }
+
+                #actions {
+                    border-top: 1px solid #ccc;
+                    display: flex;
+                    justify-content: flex-end;
+                    padding: 1rem;
+                }
+
+                #actions button {
+                    margin: 0 0.25rem;
+                }
             </style>
             <div id="backdrop"></div>
-            <div id="modal"></div>
+            <div id="modal">
+                <header>
+                    <h1>Please Confirm</h1>
+                </header>
+                <section id="main">
+                    <slot></slot>
+                </section>
+                <section id="actions">
+                    <button>Cancel</button>
+                    <button>Okay</button>
+                </section>
+            </div>
         `;
     }
 }
