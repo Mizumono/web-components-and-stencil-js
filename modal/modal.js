61792e3f9567fec1ba3733f3ad3a496fa71fa2dc
Gevorg Arzumanjan <gevorg.arzumanjan@shiwaforce.com>
2019-06-15 18:04:18 +0200

    public methods and properties

--- modal/modal.js
+++ modal/modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isOpen = false;
         this.shadowRoot.innerHTML = `
             <style>
                 #backdrop {
@@ -81,20 +82,25 @@ class Modal extends HTMLElement {
         `;
     }
 
-    // attributeChangedCallback(name, oldValue, newValue) {
-    //     if (name === 'opened') {
-    //         if (this.hasAttribute('opened')) {
-    //             this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-    //             this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-    //             this.shadowRoot.querySelector('#modal').style.opacity = 1;
-    //             this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-    //         }
-    //     }
-    // }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (this.hasAttribute('opened')) {
+            this.isOpen = true;
+            // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+            // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+            // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+            // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+        } else {
+            this.isOpen = false;
+        }
+    }
 
-    // static get observedAttributes() {
-    //     return ['opened'];
-    // }
+    static get observedAttributes() {
+        return ['opened'];
+    }
+
+    open() {
+        this.setAttribute('opened', '');
+    }
 }
 
 customElements.define('uc-modal', Modal);
